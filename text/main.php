7e23fba9b8c6ef68571e405a9423be11e883c256
forcherish <wang1057288373@163.com>
2019-08-23 20:33:48 +0800

    test

---
+++ text/main.php
@@ -0,0 +1,8 @@
+<?php
+require_once "text.php";
+
+$dog = new Animal();
+$dog->name = "dog";
+$dog->age = 2;
+$dog->foot = 4;
+$dog->jiao();
---
+++ text/text.php
@@ -0,0 +1,29 @@
+<?php
+// a标签跳转
+// $usr = $_GET['username'];
+// $pas = $_GET['password'];
+
+// echo "$usr"
+// echo "<br>"
+
+// $usr = $_POST['username'];
+// $pas = $_POST['password'];
+
+// echo "$usr<br>$pas"
+
+
+// public 公有的
+// previte 私有的
+// extends 继承                                
+class text {
+    public $name;
+    public $age;
+    public $foot;
+    public function jiao()
+    {
+        #code...
+        echo "我叫$this->name ";
+    }
+}
+ 
+?>
